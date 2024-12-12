91bfcbecba3812dbdfc1efb27c94925b52ea52c0
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2024-12-12 18:54:59 +0100

    Projekt - Laboratorium 5

---
+++ components/SideBar.jsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+
+function SideBar({ children }) {
+  const { user } = useContext(AuthProvider);
+  return (
+    <>
+      <div className="drawer drawer-open">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {children}
+          {/* Page content here */}
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-60 p-4">
+            {/* Sidebar content here */}
+            <li>
+              <Link href="/user/login">Login</Link>
+            </li>
+            <li>
+              <Link href="/user/logout">Logout</Link>
+            </li>
+            <li>
+              <Link href="/user/profile">{user}</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SideBar;
